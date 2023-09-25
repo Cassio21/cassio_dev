7a022ecad5804e65ce912bf7a8246a9493cb4246
Cássio Tenório <cassiotenoriosc@gmail.com>
2023-09-25 09:37:40 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 //**Estaticos */
 import "./NavBar.css";
 // import Pic from "../assets/Pic.jpeg";
-import logo from "../Cassio_dev/logo.png";
+import logo from "../Cassio_dev/Logotipo Desenvolvedor Web.png";
 
 const NavBar = () => {
   const [menuAberto, setMenuAberto] = useState(false);
